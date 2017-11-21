769993798db1b60b400e4bfb5f07a272b40cc20b
Sandesh Kumar <dev.skg@gmail.com>
2017-11-21 10:02:13 +0530

    autorefresh

--- stockprice/src/main/java/com/sharemarket/controller/HomeController.java
+++ stockprice/src/main/java/com/sharemarket/controller/HomeController.java
@@ -1,14 +1,19 @@
 package com.sharemarket.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RefreshScope
 public class HomeController {
+
+    @Value("${message}")
+    private String message;
 
     @RequestMapping("/index")
     public String Index(){
-        return "Hello Springboot...!!";
+        return "Hello"+ message + "...!!";
     }
 }
